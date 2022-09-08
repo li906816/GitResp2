519839b56abbb69d251b2606a11c94d3acfde842
ligw <501794830@qq.com>
2022-09-08 15:07:59 +0800

    删除头部标签

--- TestModule1/src/com/lgw/Student.java
+++ TestModule1/src/com/lgw/Student.java
@@ -1,12 +1,5 @@
 package com.lgw;
 
-/**
- * @Author: GaoWei.Li
- * @Date: 2022/9/8-15:01
- * @Description: com.lgw
- * @version: 1.0
- */
-
 public class Student {
     private String name;
     private int age;
